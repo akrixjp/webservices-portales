74eb1afed9c68f798688862041b97247217c0a50
Jorge <jorgegpinedo26@gmail.com>
2018-02-18 21:42:40 -0500

    Primer Commit

---
+++ src/main/java/pe/edu/sise/WebservicePortalesApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.sise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicePortalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicePortalesApplication.class, args);
+	}
+}
